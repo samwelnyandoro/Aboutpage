d697f1a083704dc6fcd9942241ebf190fcd92e16
TCL <kevin.okioga88@gmail.com>
2022-07-14 13:11:22 +0300

    better cover quality

--- app/src/main/java/com/aboutpage/aboutpage/MainActivity.java
+++ app/src/main/java/com/aboutpage/aboutpage/MainActivity.java
@@ -3,12 +3,60 @@ package com.aboutpage.aboutpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.aboutpage.aboutuslibrary.AboutPage;
+import com.aboutpage.aboutuslibrary.Element;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise with us");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.filee)
+                .addItem(new Element().setTitle("Version 1.0.0"))
+                .addItem(adsElement)
+                .addGroup("Connect with us")
+                .addEmail("samwelnyandoro1@gmail.com")
+                .addWebsite("https://samwelnyandoro1.netlify.app/")
+                .addFacebook("sammy.stennet.52")
+                .addTwitter("samwelnyandoro1")
+                .addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
+                .addPlayStore("com.allhub.allhub")
+                .addInstagram("samwelnyandoro1")
+                .addGitHub("samwelnyandoro")
+                .addItem(getCopyRightsElement())
+                .create();
+
+        setContentView(aboutPage);
+    }
+
+
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        copyRightsElement.setTitle(copyrights);
+        copyRightsElement.setIconDrawable(R.drawable.about_icon_copy_right);
+        copyRightsElement.setAutoApplyIconTint(true);
+        copyRightsElement.setIconTint(com.aboutpage.aboutuslibrary.R.color.about_item_icon_color);
+        copyRightsElement.setIconNightTint(android.R.color.white);
+        copyRightsElement.setGravity(Gravity.CENTER);
+        copyRightsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, copyrights, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyRightsElement;
     }
 }
